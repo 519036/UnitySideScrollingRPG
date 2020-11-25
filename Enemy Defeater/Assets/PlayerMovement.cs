a4b5c5c24a8ec45d945632d2ef7475d397879f37
519036 <519036@gmail.escg.ac.uk>
2020-11-25 22:32:36 +0000

    First Commit
    yup

---
+++ Enemy Defeater/Assets/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    public Animator anim;
+    
+    public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    float mx;
+
+    private void Update() {
+        mx = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+
+        
+
+        if (Mathf.Abs(mx) > 0.05f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        anim.SetBool("isGrounded", IsGrounded());
+    
+
+     {
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -2f;
+        }
+
+      if (Input.GetAxis("Horizontal") > 0)
+       {
+       characterScale.x = 2f;
+       }
+       transform.localScale = characterScale;
+     }
+
+        //{ 
+        //Vector3 characterScale = transform.localScale;
+        //if (Input.GetAxis("Vertical") < 0)
+        //{
+        //    characterScale.y = -5f;
+        //}
+
+       // if (Input.GetAxis("Vertical") > 0)
+        //{
+        //    characterScale.y = 5f;
+        //}
+       // transform.localScale = characterScale;//
+    //}
+}
+
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    
+}
